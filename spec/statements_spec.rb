4c9c7bc72e46ab0ea8774fe849bb2f515723a700
Ed <lookatemail@gmail.com>
2020-06-17 15:59:29 +0100

    Refactor statements spec

--- spec/statements_spec.rb
+++ spec/statements_spec.rb
@@ -3,10 +3,10 @@ require 'statements'
 describe Statements do
 
   let(:statements) { described_class.new }
+  let(:transaktions) { ['14/01/2012 || || 500.00 || 2500.00'] }
 
   it 'should be able to print user statement' do
-    transaktions = ['14/01/2012 || || 500.00 || 2500.00']
-    expect(statements.print_balance(transaktions)).to eq "date || credit || debit || balance, 14/01/2012 || || 500.00 || 2500.00"
+    expect(statements.print_balance(transaktions)).to eq "date || credit || debit || balance, #{transaktions.join(', ')}"
   end
 
 end
