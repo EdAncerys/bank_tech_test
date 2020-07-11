ceb599cac152032320d7cef9b17245b215ee7b84
Ed <lookatemail@gmail.com>
2020-07-11 18:06:24 +0100

    Refactor spec file

--- JS/spec/BankSpec.js
+++ JS/spec/BankSpec.js
@@ -6,26 +6,28 @@ describe('Bank', function () {
     bank = new Bank();
   });
 
-  it('current balance at start should be eq 0', () => {
-    expect(bank.currentBalance()).toEqual(0);
+  describe('Bank transaction', () => {
+    it('current balance at start should be eq 0', () => {
+      expect(bank.currentBalance()).toEqual(0);
+    });
+
+    it('should be able to top up current balance by 100', () => {
+      expect(bank.depositToAccount(100)).toEqual(100);
+    });
+
+    it('should be able to withdraw from account', () => {
+      bank.depositToAccount(150);
+      expect(bank.withdrawFromAccount(50)).toEqual(100);
+    });
+
+    it('should be able to top up and withdraw from account', () => {
+      bank.depositToAccount(200);
+      bank.withdrawFromAccount(100);
+      expect(bank.currentBalance()).toEqual(100);
+    });
   });
 
-  it('should be able to top up current balance by 100', () => {
-    expect(bank.depositToAccount(100)).toEqual(100);
-  });
-
-  it('should be able to withdraw from account', () => {
-    bank.depositToAccount(150);
-    expect(bank.withdrawFromAccount(50)).toEqual(100);
-  });
-
-  it('should be able to top up and withdraw from account', () => {
-    bank.depositToAccount(200);
-    bank.withdrawFromAccount(100);
-    expect(bank.currentBalance()).toEqual(100);
-  });
-
-  describe('Cathing errors', () => {
+  describe('Catching errors', () => {
     it('should be able to throw error if deposited amount is negative', () => {
       expect(() => {
         bank.depositToAccount(-100);
@@ -40,19 +42,21 @@ describe('Bank', function () {
     });
   });
 
-  it('should be able to print account balance after deposit', () => {
-    bank.depositToAccount(100);
-    expect(bank.printStatement()).toEqual({
-      'Transaktion ID 0': `Deposit: 100 Balance: 100 Date: ${dateNow}`,
+  describe('Printing balance', () => {
+    it('should be able to print account balance after deposit', () => {
+      bank.depositToAccount(100);
+      expect(bank.printStatement()).toEqual({
+        'Transaktion ID 0': `Deposit: 100 Balance: 100 Date: ${dateNow}`,
+      });
     });
-  });
 
-  it('should be able to print account balance after withdrawal', () => {
-    bank.depositToAccount(100);
-    bank.withdrawFromAccount(30);
-    expect(bank.printStatement()).toEqual({
-      'Transaktion ID 0': `Deposit: 100 Balance: 100 Date: ${dateNow}`,
-      'Transaktion ID 1': `Withdrawal: 30 Balance: 70 Date: ${dateNow}`,
+    it('should be able to print account balance after withdrawal', () => {
+      bank.depositToAccount(100);
+      bank.withdrawFromAccount(30);
+      expect(bank.printStatement()).toEqual({
+        'Transaktion ID 0': `Deposit: 100 Balance: 100 Date: ${dateNow}`,
+        'Transaktion ID 1': `Withdrawal: 30 Balance: 70 Date: ${dateNow}`,
+      });
     });
   });
 });
